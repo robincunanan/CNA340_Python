028e8e72bdea1a96ae5ecc4403dc0a8d297fe267
robincunanan <rtcunanan@student.rtc.edu>
2019-05-17 04:26:52 -0700

    Test
    Test

---
+++ Problem #1.py
@@ -0,0 +1,7 @@
+name = input('Please enter your name: ')
+location = input('Enter a location of your choice: ')
+number = input('Enter any number: ')
+plural = input('Enter a plural noun such as "Cats", with an "s": ')
+
+print(name + " went to " + location + " to buy " + str(number) + " different types of " + plural + ".")
+
